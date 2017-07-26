28a696f4cb468b98f31a5a43a3e43da6a5fc4732
murilofragoso <mu_fragoso@hotmail.com>
2017-07-26 16:47:16 -0300

    desenvolvendo arquivos de media

---
+++ Media2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Media2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+            int x = 0 ;
+            while (true)
+            {
+
+                Console.WriteLine("insira um valor. 0 para encerrar");
+                int y = int.Parse(Console.ReadLine());
+                if (y == 0)
+                    break;
+                valor += y;
+                x += 1;
+            }
+            int media = valor / x;
+            Console.WriteLine("a media dos valores e: {0} ", media);
+
+            Console.ReadKey();
+        }
+    }
+}
